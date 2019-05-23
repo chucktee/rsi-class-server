0079df31d1696a7f47561406d81f62af37d25554
Chuck Toussieng <chuckt@chuckt.com>
2019-05-23 17:09:50 -0400

    WIP

---
+++ api_server_1.js
@@ -0,0 +1,57 @@
+// Richwood Scientific Bootcamp
+// Basic Node+Express server for
+// our test company
+const express = require("express");
+const app = express();
+
+// Set the port to listen on. 3000 in this case
+app.set("port", process.env.PORT || 3000);
+
+// Add BodyParser to read HTTP message body
+const bodyParser = require("body-parser")
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Morgan for logging
+const morgan = require("morgan");
+app.use(morgan(":date :remote-addr :method :url :status :response-time ms - :res[content-length]"));
+
+//-----------------------------------------
+// Startup the server
+app.listen(app.get("port"), function(){
+	console.log( "The API Server is running at http://localhost:" + app.get("port"));
+});
+
+module.exports = app;
+
+
+
+//-----------------------------------------
+// API routes
+//
+
+//-----------------------------------------
+// Play Functions
+
+const play = require("./routes/play");
+
+// App Version
+app.get("/api/version", play.version);
+
+// Add Two Version
+app.get("/api/add-two/:x/:y", play.addTwo);
+
+// Make an HTML mailing label
+app.post("/api/make-mailer", play.makeHTMLMailingLabel);
+
+//-----------------------------------------
+
+
+//-----------------------------------------
+// Finally If no routes match, send 404
+app.use(function(req,res) {
+	res.status(404).json({result: 'not found', data:{}});
+});
+
---
+++ routes/play.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//-------------------------------------------------------------------------------------------
+// GET Version
+exports.version = function(req, res) {
+	res.status(200).json({"result": "success", "data":{ "version": "1.0" }});		
+};
+
+//-------------------------------------------------------------------------------------------
+// GET Add two numbers
+exports.addTwo = function(req, res) {
+
+	let answer = req.params.x + req.params.y;
+
+	res.status(200).json({"result": "success", "data":{ "answer": answer }});		
+};
+
+//-------------------------------------------------------------------------------------------
+// POST Format a mailing label
+exports.makeHTMLMailingLabel = function(req, res) {
+
+	let label = "";
+
+	label += req.body.data.first_name + " " + req.body.data.last_name + "</br>";
+	label += req.body.data.street_number + " " + req.body.data.street_name + "</br>";
+	label += req.body.data.city + ", " + req.body.data.state + " " + req.body.data.zipcode + "</br>";
+
+	res.status(200).json({"result": "success", "data":{ "label": label }});		
+};
+
